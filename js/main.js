ac56b6c48d711096ae6320e1a339290907798c48
nathan anderson <anderson@nathanworking.com>
2016-05-09 19:35:27 -0400

    Style Edits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+var $caption = $("<p></p>");
+
+
+$(document).ready(function(){
+	$('.gallery').featherlightGallery();
+
+  //Get child's title attribute and set caption
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+
 //----------------------------------------------------------------//
 //---------------// Calling Hideseek Live Search //---------------//
 //----------------------------------------------------------------//
