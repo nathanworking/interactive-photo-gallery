7ba973871d132ae2a3e9519ddcf514e37acc58b1
nathan anderson <anderson@nathanworking.com>
2016-05-10 17:57:36 -0400

    Different Lightbox Method
    Found a new plugin, installed that.
    
    Error: when changing views

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 //----------------------------------------------------------------//
-//------------// Showing Captions on Lightbox View //------------//
+//---------------// Calling Hideseek Live Search //---------------//
 //----------------------------------------------------------------//
-$(document).ready(function(){
-	var $caption = $("<p></p>");
-	//Get child's title attribute and set caption
-	var captionText = $('a figcaption');
-	$caption.text(captionText);
-
-	//A caption to overlay
-	$('div .featherlight-content').append($caption);
-});
+	$('#lightgallery').lightGallery({
+		download: false,
+		counter: false,
+		zoom: false,
+		thumbnail: false,
+		mode: 'lg-fade'
+	});
 
 //----------------------------------------------------------------//
 //---------------// Calling Hideseek Live Search //---------------//
 //----------------------------------------------------------------//
 $(document).ready(function() {
-    $('#search').hideseek();
+  $('#search').hideseek({
+		nodata: 'No results found'
+	});
 });
